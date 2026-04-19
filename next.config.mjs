8e7503345e3959a2cd5bbae0f0f83188e1a5f2c3
amboy <nyomana369@gmail.com>
2026-04-19 17:34:10 +0700

    chore: update

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,13 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"],
+      allowedOrigins: process.env.NEXT_PUBLIC_APP_URL
+        ? [new URL(process.env.NEXT_PUBLIC_APP_URL).host]
+        : ["localhost:3000"],
     },
+  },
+  turbopack: {
+    root: ".",
   },
 };
 
